82902f6a6ada2a613b87267a1daee250cb968921
Lacctoff <lasisitofunmi@gmail.com>
2026-01-09 00:01:06 +0100

    integrated validation using zod, completed middleware functions

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,13 +1,19 @@
 import express from 'express'
-import { addToWatchlist, removeFromWatchlist } from '../controllers/watchlistController.js';
+import { addToWatchlist, removeFromWatchlist, updateWatchlistItem } from '../controllers/watchlistController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
+import { validateRequest } from '../middleware/validateRequest.js';
+import { addToWatchlistSchema } from '../validators/watchlistValidators.js';
 
 const router = express.Router()
 
 router.use(authMiddleware);
 
-router.post('/', addToWatchlist);
+router.post('/', validateRequest(addToWatchlistSchema), addToWatchlist);
 
+// {{baseUrl}}/watchlist/:id
+router.put('/:id', updateWatchlistItem);
+
+// {{baseUrl}}/watchlist/:id
 router.delete('/:id', removeFromWatchlist);
 
 export default router;
